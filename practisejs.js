107fee35ff6f238592787ec8ccb12ed4e618db73
Nikhills265 <niknikhil265@gmail.com>
2024-11-07 00:38:13 +0530

    first commit

---
+++ practisejs.js
@@ -0,0 +1,74 @@
+/*  var keyword*/
+var a = 5;
+var b = 6;
+var c = a + b ;
+console.log(c)
+/* constant keyword*/
+const pi=3.14;
+var f=5;
+console.log(c)
+console.log(pi)
+{
+    let d=10;
+    console.log(d)
+}
+console.log(c)
+console.log(pi)
+
+let name = 'john';
+const age = 30;
+var isStudent = true;
+let numbers = [1,2,3,4,5];
+let person = { name: "Alice", age: 25};
+
+function fn()
+{
+    console.log("function executed")   
+}
+/* function call */
+fn();
+fn();
+/* let keyword- Installing the vaariable  */
+{ let d=10
+    console.log(d)
+}
+console.log(c)
+console.log(pi)
+/* function defination */
+function fn()
+{
+    console.log("function executed")
+}
+fn();
+
+/* Another One */
+let num =10;
+if (num >0);
+{ 
+    console.log( "Positive Number");
+    
+}
+
+/*for initinilizatiion; condition; inc/dec  */
+
+for(let i = 0; i < 5 ; i++)
+{
+    console.log("iteration"+ i)
+}
+
+/*increment*/  
+var ab=5 
+console.log(--ab);      
+
+/*array */
+let fruits = ["apple", "banana", "organe" ];
+fruits.push("grape");
+fruits.push("pineapple");
+fruits.pop();
+console.log(fruits);
+
+/*objects */
+let individual= {name:"Alice",age:25}
+individual.age=26;
+
+/* */
